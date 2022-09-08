6a7f08e431bd88df936612ec3bb97a72037d82a6
Daniele Zelante <zeldan@zetasys.net>
2022-09-08 12:55:42 -0400

    MIT license into files javadoc structure

--- src/main/java/net/zetasys/zava/db/Zql.java
+++ src/main/java/net/zetasys/zava/db/Zql.java
@@ -1,6 +1,24 @@
 /*
- * YAL zeldan
- */
+Copyright 2022 Daniele Zelante  <zeldan@zetasys.net>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 package net.zetasys.zava.db;
 
 import java.math.BigDecimal;
@@ -12,6 +30,10 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import org.javamoney.moneta.Money;
 
+/**
+ * JDBC SQL wrapper to allow transparent conversion of java-null to SQL-NULL
+ * @author zeldan
+ */
 public class Zql
 {
 
@@ -39,6 +61,13 @@ public class Zql
         return rs.wasNull() ? null : x;
     }
 
+    /**
+     * set Double value in statement, converting java-null to SQL-NULL
+     * @param st PreparedStatement to use
+     * @param field field index (1-based)
+     * @param value value to set (null is allowed)
+     * @throws SQLException
+     */
     public static void setDouble(PreparedStatement st, int field, Double value) throws SQLException
     {
         if (value == null)
