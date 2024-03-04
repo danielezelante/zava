629adaaf16235d770edd3138877ee9fcdfafefce
Daniele Zelante <zeldan@zetasys.net>
2024-03-04 10:53:20 -0300

    copyright

--- src/main/java/net/zetasys/zava/db/Office.java
+++ src/main/java/net/zetasys/zava/db/Office.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Daniele Zelante  <zeldan@zetasys.net>
+Copyright 2022-2024 Daniele Zelante  <zeldan@zetasys.net>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
